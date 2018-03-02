2a1c260a085fe0b2369fc63028b2bde85d3cb135
Daniel Brito <daniell-henrique@hotmail.com>
2018-03-01 23:36:08 -0300

    Add files via upload

---
+++ pense-em-python_allen-downey/Capítulo 1/exe1.2.py
@@ -0,0 +1,9 @@
+# 1.2.1 - Quantos segundos há em 42 minutos e 42 segundos?
+
+print(42*60)+42
+
+# 1.2.2 - Quantas milhas há em 10 quilômetros? Dica: uma milha equivale a 1.61 quilômetro.
+
+print(10/1.61)
+
+# 1.2.3 - Se você correr 10 quilômetros em 42 minutos e 42 segundos, qual é o seu passo médio (tempo por milha em minutos e segundos)? Qual é a sua velocidade média em milhas por hora?
