c18c6e687252389a35c91bb4acfeb55b7b07fff4
Daniel Brito <daniell-henrique@hotmail.com>
2019-01-24 14:10:46 -0200

    Add files via upload

--- java-basico_loiane-groner/Aula 52/TestaAgenda.java
+++ java-basico_loiane-groner/Aula 52/TestaAgenda.java
@@ -3,37 +3,62 @@ package com.daniel.cursojava.aula52;
 import java.util.Scanner;
 
 public class TestaAgenda {
+	
+	public static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
         Agenda agenda = new Agenda();
+        int opcao = 0;
         
-        int opcao = 1;
-        
-        while (opcao != 3){
+        while(opcao!=3) {
         	
-            opcao = obterOpcaoMenu(scan);
+        	do {
+        		
+        		System.out.println("\nAGENDA\n");
+                System.out.println("1 - Consultar contato");
+                System.out.println("2 - Adicionar contato");
+                System.out.println("3 - Sair");
+                
+                System.out.print("\n- > ");
+                
+                try {
+                	
+                    String entrada = scan.nextLine();
+                    opcao = Integer.parseInt(entrada);
+                    
+                    if(opcao!=1 && opcao!=2 && opcao!=3){
+                    	
+                    	throw new Exception("\nEntrada inválida!");
+                    }
+                } 
+                catch (Exception e){
+                	
+                    System.out.println("\nEntrada inválida, digite novamente.");
+                }
+                
+            }while(opcao!=1 && opcao!=2 && opcao!=3);
+        	
+        	switch(opcao) {
             
-            if(opcao == 1){
-            	
-                consultarContato(scan, agenda);
-            } 
-            else 
-            if(opcao == 2){
-            	
-                adicionarContato(scan, agenda);
-            }
-        }
+	        	case 1: consultarContato(agenda); break;
+	        	case 2: adicionarContato(agenda); break;
+	        }
+        } 
 	}
 	
-	public static void adicionarContato(Scanner scan, Agenda agenda){
+	public static void adicionarContato(Agenda agenda){
 		
         try {
         	
-            String nome = lerDados(scan, "\nDigite o nome: ");
-            String telefone = lerDados(scan, "Digite o telefone: ");
-            String email = lerDados(scan, "Digite o email: ");
+        	System.out.print("\nDigite o nome: ");
+        	String nome = scan.nextLine();
+        	
+        	System.out.print("Digite o telefone: ");
+        	String telefone = scan.nextLine();
+        	
+        	System.out.print("Digite o email: ");
+        	String email = scan.nextLine();
             
             Contato contato = new Contato(nome, telefone, email);
             
@@ -45,9 +70,10 @@ public class TestaAgenda {
         }
     }
     
-    public static void consultarContato(Scanner scan, Agenda agenda){
+    public static void consultarContato(Agenda agenda){
     	
-        String nomeContato = lerDados(scan, "\nDigite o nome: ");
+    	System.out.print("\nDigite o nome: ");
+    	String nomeContato = scan.nextLine();
             
         try {
         	
@@ -62,50 +88,5 @@ public class TestaAgenda {
         	
         	System.out.println(e.getMessage());
         }
-    }
-    
-    public static String lerDados(Scanner scan, String msg){
-    	
-        System.out.print(msg);
-        String entrada = scan.nextLine();
-        
-        return entrada;
-    }
-    
-    public static int obterOpcaoMenu(Scanner scan){
-        
-        boolean entradaValida = false;
-        int opcao = 3;
-        
-        while (!entradaValida){
-        	
-            System.out.println("\nAGENDA\n");
-            System.out.println("1 - Consultar contato");
-            System.out.println("2 - Adicionar contato");
-            System.out.println("3 - Sair");
-            
-            System.out.print("\n- > ");
-        
-            try {
-            	
-                String entrada = scan.nextLine();
-                opcao = Integer.parseInt(entrada);
-                
-                if (opcao == 1 || opcao == 2 || opcao == 3){
-                	
-                    entradaValida = true;
-                } 
-                else {
-                	
-                    throw new Exception("\nEntrada inválida!");
-                }
-            } 
-            catch (Exception e){
-            	
-                System.out.println("\nEntrada inválida, digite novamente.");
-            }
-        }
-        
-        return opcao;
     }
 }
